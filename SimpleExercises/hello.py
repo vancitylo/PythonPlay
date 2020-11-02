44e35ec9b38d7635a62a34fc45a4275efa644ba1
Administrator <administrator@Administrators-MacBook-Air.local>
2020-11-02 16:16:37 +0100

    nov2 4pm

--- SimpleExercises/hello.py
+++ SimpleExercises/hello.py
@@ -1,1 +1,9 @@
-print("hello, cruel world")
+length = float(input("What is the length of your space?"))
+width = float(input("What is the width of your space?"))
+height = float(input("What is the height of your space?"))
+
+area = length*width
+vol = length*width*height
+
+info = "The area of your space is {0:.2f}m2 and the volume of your space is {1:.2f}m3"
+print(info.format(area,vol))
