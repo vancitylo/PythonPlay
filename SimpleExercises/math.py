761219d4c64472a7450b9629f7f12153bafa00df
Administrator <administrator@Administrators-MacBook-Air.local>
2020-11-02 16:36:27 +0100

    math update

---
+++ SimpleExercises/math.py
@@ -0,0 +1,7 @@
+energy = int(input("How much energy do you have (from 1-10)?"))
+
+if energy <5:
+    print("Get some coffee and stretch.")
+elif energy >4 and energy <8:
+    print("Go for a walk.")
+else: print("Lucky fucking you.")
